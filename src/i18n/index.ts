98a0bbbe599299410c06efbeea4809c20894a341
fuyoo <fuyoo0928@gmail.com>
2022-10-20 15:06:09 +0800

    add lang

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,15 +1,23 @@
 import chinese from "@/i18n/chinese";
 import en from "@/i18n/en";
-export const langs = [{lang: "简体中文", value: "chinese"}, {
-  lang: "English",
-  value: "en"
-}];
+export const langs = {
+  "chinese": {
+    lang: "简体中文",
+    value: "chinese",
+    source: chinese
+  },
+  "en": {
+    lang: "English",
+    value: "en",
+    source: en
+  }
+};
 const local = localStorage.getItem("lang") || "chinese";
 export const lang = (param: string) => {
-  return eval(`${local}.${param}`);
+  return eval(`langs.${local}.source.${param}`);
 };
 export default {
   install(app: any) {
-    app.config.globalProperties.$lang = lang
+    app.config.globalProperties.$lang = lang;
   }
 };
