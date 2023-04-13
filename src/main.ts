c797b6e68fce5022a3c902f3991e319992b0228b
fuyoo <fuyoo0928@gmail.com>
2023-04-13 16:23:36 +0800

    format

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import {createApp} from "vue";
-import {createPinia} from "pinia";
+/**
+ * vue3 project main.ts
+ */
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 import Suspenser from "./SuspenserView.vue";
 import router from "./router";
 import i18n from "@/i18n";
@@ -8,5 +11,11 @@ import "normalize.css";
 import "element-plus/dist/index.css";
 import "element-plus/theme-chalk/dark/css-vars.css";
 import "@/style/common.scss";
+
 const app = createApp(Suspenser);
-app.use(createPinia()).use(i18n).use(router).use(ElementPlus, {size: "small"}).mount("#app");
+app
+  .use(createPinia())
+  .use(i18n)
+  .use(router)
+  .use(ElementPlus, { size: "small" })
+  .mount("#app");
